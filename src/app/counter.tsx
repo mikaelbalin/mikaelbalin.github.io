43a41752bff76b97b76c9c476ebf18558eaee053
Mikhail Balin <michbalin@gmail.com>
2024-03-28 16:32:40 +0000

    Add stylex and global CSS import

--- src/app/counter.tsx
+++ src/app/counter.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { useState } from "react";
+import stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  foo: {
+    color: "green",
+  },
+  bar: {
+    backgroundColor: "blanchedalmond",
+  },
+});
 
 export default function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div {...stylex.props(styles.foo, styles.bar)}>
       <p>You clicked {count} times</p>
       <button
         onClick={() => {
