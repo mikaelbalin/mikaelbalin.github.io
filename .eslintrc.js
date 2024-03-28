1d4c19584b7e93379274fede6af36a0f52ef0a23
Mikhail Balin <michbalin@gmail.com>
2024-03-28 18:26:29 +0000

    Add Storybook configuration and scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: "next/core-web-vitals",
+  extends: ["next/core-web-vitals", "plugin:storybook/recommended"],
   plugins: ["@stylexjs"],
   rules: {
     "@stylexjs/valid-styles": "error",
