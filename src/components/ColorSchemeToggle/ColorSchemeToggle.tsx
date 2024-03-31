190fe97087d40f5e540f50330a051c2fc1d6de1d
Mikhail Balin <michbalin@gmail.com>
2024-03-31 18:30:38 +0100

    Add ColorSchemeToggle, Footer, and HeaderMenu components

---
+++ src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Switch, useMantineTheme, rem } from "@mantine/core";
+import { IconCheck, IconX } from "@tabler/icons-react";
+
+function Demo() {
+  const theme = useMantineTheme();
+  const [checked, setChecked] = useState(false);
+  return (
+    <Switch
+      checked={checked}
+      onChange={(event) => setChecked(event.currentTarget.checked)}
+      color="teal"
+      size="md"
+      label="Switch with thumb icon"
+      thumbIcon={
+        checked ? (
+          <IconCheck
+            style={{ width: rem(12), height: rem(12) }}
+            color={theme.colors.teal[6]}
+            stroke={3}
+          />
+        ) : (
+          <IconX
+            style={{ width: rem(12), height: rem(12) }}
+            color={theme.colors.red[6]}
+            stroke={3}
+          />
+        )
+      }
+    />
+  );
+}
