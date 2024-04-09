5d543867420545be0c0bde74de1b9452ecf3eca5
Mikhail Balin <michbalin@gmail.com>
2024-04-09 17:02:38 +0100

    Refactor HeroBackground component and update canvas animation

--- src/components/HeroBackground/HeroBackground.tsx
+++ src/components/HeroBackground/HeroBackground.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useRef } from "react";
 
 const SQUARE_SIZE = 60;
+const ANIMATION_DURATION = 2000;
 
 const lerp = (start: number, end: number, t: number) =>
   start * (1 - t) + end * t;
@@ -22,103 +23,104 @@ class Square {
   }
 
   draw(context: CanvasRenderingContext2D) {
-    context.beginPath();
-    context.rect(this.xPos, this.yPos, this.width, this.height);
-    context.closePath();
-
     context.fillStyle = `rgba(240, 237, 231, ${this.opacity})`;
-    context.fill();
+    context.fillRect(this.xPos, this.yPos, this.width, this.height);
   }
 }
 
+const getRandomSquares = (squares: Square[]) => {
+  let squaresCopy = [...squares];
+  for (let i = squaresCopy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [squaresCopy[i], squaresCopy[j]] = [squaresCopy[j], squaresCopy[i]];
+  }
+  return squaresCopy.slice(0, Math.floor(Math.random() * 11));
+};
+
+const drawSquares = (
+  ctx: CanvasRenderingContext2D,
+  canvas: HTMLCanvasElement
+) => {
+  const numX = Math.ceil(canvas.width / SQUARE_SIZE);
+  const numY = Math.ceil(canvas.height / SQUARE_SIZE);
+
+  return Array.from({ length: numX * numY }, (_, i) => {
+    const x = (i % numX) * SQUARE_SIZE;
+    const y = Math.floor(i / numX) * SQUARE_SIZE;
+    const square = new Square(x, y);
+    square.draw(ctx);
+    return square;
+  });
+};
+
+const resizeCanvas = (
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D
+) => {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
+
+  return drawSquares(ctx, canvas);
+};
+
 export const HeroBackground = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const squares = useRef<Square[]>([]);
   const animationFrameId = useRef<number | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    if (!canvas) return;
+    const ctx = canvas?.getContext("2d");
 
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+    if (!canvas || !ctx) return;
 
-    function getRandomSquares() {
-      let squaresCopy = [...squares.current];
-      for (let i = squaresCopy.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [squaresCopy[i], squaresCopy[j]] = [squaresCopy[j], squaresCopy[i]];
-      }
-      return squaresCopy.slice(0, Math.floor(Math.random() * 11));
-    }
+    let squares = resizeCanvas(canvas, ctx);
 
     const animate = () => {
       let start: DOMHighResTimeStamp | null = null;
-      let currentSquares: Square[] = getRandomSquares();
+      let randomSquares: Square[] = getRandomSquares(squares);
 
-      const redraw = (time: DOMHighResTimeStamp) => {
+      const redraw = (time: DOMHighResTimeStamp = 0) => {
         if (!start) {
           start = time;
         }
 
         const elapsed = time - start;
 
-        currentSquares.forEach((currentSquare) => {
-          ctx.clearRect(
-            currentSquare.xPos,
-            currentSquare.yPos,
-            currentSquare.width,
-            currentSquare.height
+        randomSquares.forEach((square) => {
+          ctx.clearRect(square.xPos, square.yPos, square.width, square.height);
+          square.opacity = lerp(
+            0,
+            1,
+            Math.min(1, elapsed / ANIMATION_DURATION)
           );
-          currentSquare.opacity = lerp(0, 1, Math.min(1, elapsed / 2000));
-          currentSquare.draw(ctx);
+          square.draw(ctx);
         });
 
-        if (elapsed < 2000) {
+        if (elapsed < ANIMATION_DURATION) {
           animationFrameId.current = requestAnimationFrame(redraw);
         } else {
-          currentSquares = getRandomSquares();
+          randomSquares = getRandomSquares(squares);
           start = null;
-          redraw(0);
+          redraw();
         }
       };
 
-      redraw(0);
+      redraw();
     };
 
-    const drawSquares = () => {
-      const numX = Math.ceil(canvas.width / SQUARE_SIZE);
-      const numY = Math.ceil(canvas.height / SQUARE_SIZE);
-
-      squares.current = [];
-
-      for (let i = 0; i < numX; i++) {
-        for (let j = 0; j < numY; j++) {
-          const circle = new Square(i * SQUARE_SIZE, j * SQUARE_SIZE);
-          squares.current.push(circle);
-          circle.draw(ctx);
-        }
-      }
+    const handleResize = () => {
+      squares = resizeCanvas(canvas, ctx);
     };
-
-    const resizeCanvas = (e?: UIEvent) => {
-      canvas.width = document.documentElement.clientWidth;
-      canvas.height = document.documentElement.clientHeight;
-
-      drawSquares();
-    };
-
-    resizeCanvas();
     animate();
 
-    window.addEventListener("resize", resizeCanvas, false);
+    window.addEventListener("resize", handleResize, false);
     return () => {
-      window.removeEventListener("resize", resizeCanvas, false);
+      window.removeEventListener("resize", handleResize, false);
       if (animationFrameId.current !== null) {
         cancelAnimationFrame(animationFrameId.current);
       }
     };
   }, []);
 
-  return <canvas ref={canvasRef} height={800} />;
+  return <canvas ref={canvasRef} />;
 };
