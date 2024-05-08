9174e8e92b44dd3d7ef9f1647e0760b9063dc4ee
Mikhail Balin <michbalin@gmail.com>
2024-05-08 13:24:21 +0100

    Update ColorSchemeToggle styles and icons

--- src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,15 +1,13 @@
 import {
   Switch,
-  useMantineTheme,
-  rem,
   useMantineColorScheme,
   useComputedColorScheme,
 } from "@mantine/core";
+import cx from "clsx";
 import { IconSunHigh, IconMoon } from "@tabler/icons-react";
 import classes from "./ColorSchemeToggle.module.css";
 
 export const ColorSchemeToggle = () => {
-  const theme = useMantineTheme();
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme("light", {
     getInitialValueInEffect: true,
@@ -27,19 +25,10 @@ export const ColorSchemeToggle = () => {
         thumb: classes.thumb,
       }}
       thumbIcon={
-        isLightScheme ? (
-          <IconSunHigh
-            style={{ width: rem(12), height: rem(12) }}
-            color="#101010"
-            stroke={3}
-          />
-        ) : (
-          <IconMoon
-            style={{ width: rem(12), height: rem(12) }}
-            color={theme.white}
-            stroke={3}
-          />
-        )
+        <>
+          <IconSunHigh className={cx(classes.icon, classes.dark)} stroke={3} />
+          <IconMoon className={cx(classes.icon, classes.light)} stroke={3} />
+        </>
       }
     />
   );
