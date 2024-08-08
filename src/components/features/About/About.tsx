9733d330417bab3319e27d1b81c2e1b4b2867a32
Mikael Balin <michbalin@gmail.com>
2024-08-08 12:11:06 +0300

    Refactor About component to use Tailwind CSS classes

--- src/components/features/About/About.tsx
+++ src/components/features/About/About.tsx
@@ -1,13 +1,19 @@
-import { Container, Title, Text } from "@mantine/core";
-import { AboutProps } from "../../../types/data";
+import { Container, Title, Text, Grid, GridCol } from "@mantine/core";
+import { AboutProps } from "@/types/data";
 
 export const About = (props: AboutProps) => {
   const { heading, text } = props;
 
   return (
-    <Container component="section" className="pt-25 pb-16">
-      <Title order={2}>{heading}</Title>
-      <Text className="mt-8">{text}</Text>
+    <Container component="section" className="pt-25 pb-17">
+      <Title order={2} className="mb-8">
+        {heading}
+      </Title>
+      <Grid>
+        <GridCol span={{ base: 12, sm: 6 }} offset={{ sm: 6 }}>
+          <Text size="lg">{text}</Text>
+        </GridCol>
+      </Grid>
     </Container>
   );
 };
--- src/theme/components/title/title.ts
+++ src/theme/components/title/title.ts
@@ -5,19 +5,19 @@ export default Title.extend({
     switch (order) {
       case 1:
         return {
-          root: "text-9xl md:text-10xl",
+          root: "text-9xl sm:text-10xl",
         };
       case 2:
         return {
-          root: "text-lg md:text-8xl",
+          root: "text-5xl sm:text-8xl",
         };
       case 3:
         return {
-          root: "text-3xl md:text-6xl",
+          root: "text-3xl sm:text-6xl",
         };
       case 4:
         return {
-          root: "text-xl md:text-4xl",
+          root: "text-xl sm:text-4xl",
         };
       default:
         return {
