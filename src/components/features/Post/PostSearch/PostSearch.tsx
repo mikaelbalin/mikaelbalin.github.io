e62369448b207cb715b47fde37bbf8a239540800
Mikael Balin <michbalin@gmail.com>
2024-08-24 15:20:30 +0100

    Add PostSearch component for searching blog posts

--- src/components/features/Post/PostSearch/PostSearch.tsx
+++ src/components/features/Post/PostSearch/PostSearch.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { Box, Chip, ChipGroup, Container, Group, Text } from "@mantine/core";
+import { useState } from "react";
+
 export const PostSearch = () => {
-  return <div></div>;
+  // string value when multiple is false (default)
+  const [value, setValue] = useState("all");
+
+  return (
+    <Box>
+      <Text className="mb-8">Search by category</Text>
+      <ChipGroup multiple={false} value={value} onChange={setValue}>
+        <Group>
+          <Chip value="all">All</Chip>
+          <Chip value="react">React</Chip>
+          <Chip value="ng">Angular</Chip>
+          <Chip value="svelte">Svelte</Chip>
+          <Chip value="vue">Vue</Chip>
+        </Group>
+      </ChipGroup>
+    </Box>
+  );
 };
--- src/theme/components/chip/chip.ts
+++ src/theme/components/chip/chip.ts
@@ -1,15 +1,22 @@
+import { cn } from "@/lib/utils";
 import { Chip } from "@mantine/core";
 
 export default Chip.extend({
   defaultProps: {
-    radius: 0,
-    // variant: "primary",
+    variant: "outline",
   },
-  // vars: (theme, props) => {
-  //   return {
-  //     root: {
-  //       "--chip-radius": "0",
-  //     },
-  //   };
-  // },
+  classNames: {
+    iconWrapper: "hidden",
+    label: cn(
+      "py-2 px-4 text-base !border-black h-auto transition-colors",
+      "hover:!bg-appLightColorBeige",
+      "data-[checked=true]:bg-black data-[checked=true]:text-white",
+      "hover:data-[checked=true]:!bg-appLightColorGreyDark",
+    ),
+  },
+  vars: (theme, props) => ({
+    root: {
+      "--chip-radius": "0",
+    },
+  }),
 });
