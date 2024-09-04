87163bf95aefcd02bf0417516810c2d0263f10f2
Mikael Balin <michbalin@gmail.com>
2024-09-04 14:36:37 +0100

    Fix tag filtering functionality

--- src/components/features/Post/PostSearch/PostSearch.tsx
+++ src/components/features/Post/PostSearch/PostSearch.tsx
@@ -2,27 +2,29 @@
 
 import { TagListResponseDataItem } from "@/types/data";
 import { Box, Chip, ChipGroup, Container, Group, Text } from "@mantine/core";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 interface PostSearchProps {
   tags: TagListResponseDataItem[];
 }
 
 export const PostSearch = ({ tags }: PostSearchProps) => {
+  const params = useParams<{ lang: string; tag?: string }>();
   const router = useRouter();
-  const [tag, setTag] = useState("all");
 
   function filterPosts(value: string): void {
     router.push(`/blog/tags/${value}`);
-    setTag(value);
   }
 
   return (
     <Container component="section" className="pt-17">
       <Box>
         <Text className="mb-8">Search by category</Text>
-        <ChipGroup multiple={false} value={tag} onChange={filterPosts}>
+        <ChipGroup
+          multiple={false}
+          value={params.tag || "all"}
+          onChange={filterPosts}
+        >
           <Group>
             <Chip value="all">All</Chip>
             {tags.map(({ id, attributes: { slug, name } }) => (
