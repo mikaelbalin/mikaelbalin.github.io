8d9c8a3e6a9d12085f41964e292197aac75ae3de
Mikael Balin <michbalin@gmail.com>
2024-09-07 13:22:16 +0100

    Refactor ArticleContent component to use Code component for displaying JSON data

--- src/components/features/Article/ArticleContent/ArticleContent.tsx
+++ src/components/features/Article/ArticleContent/ArticleContent.tsx
@@ -1,9 +1,13 @@
-import { Container } from "@mantine/core";
+import { Code, Container } from "@mantine/core";
 
 interface ArticleContentProps {
   data: any;
 }
 
 export const ArticleContent = ({ data }: ArticleContentProps) => {
-  return <Container>{JSON.stringify(data, null, 2)}</Container>;
+  return (
+    <Container>
+      <Code block>{JSON.stringify(data, null, 2)}</Code>
+    </Container>
+  );
 };
