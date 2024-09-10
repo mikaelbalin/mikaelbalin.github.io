8746a2a1af4f8aba88ad1a480f5f5118d8373abf
Mikael Balin <michbalin@gmail.com>
2024-09-10 18:38:57 +0100

    Add rehype-raw dependency

--- src/components/blocks/markdown-text.tsx
+++ src/components/blocks/markdown-text.tsx
@@ -1,6 +1,27 @@
 import { TypographyStylesProvider } from "@mantine/core";
-import Markdown from "react-markdown";
+import Markdown, { ExtraProps } from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import { createElement, DetailedHTMLProps } from "react";
+
+const HeadingRenderer = (
+  props: DetailedHTMLProps<
+    React.HTMLAttributes<HTMLHeadingElement>,
+    HTMLHeadingElement
+  > &
+    ExtraProps,
+) => {
+  console.log({ props });
+  const Tag = props.node?.tagName || "h1";
+  const id = props.children?.toString().replace(/\s+/g, "-").toLowerCase();
+  return createElement(
+    Tag,
+    { id },
+    <a href={`#${id}`} style={{ textDecoration: "none", color: "inherit" }}>
+      {props.children}
+    </a>,
+  );
+};
 
 interface MarkdownTextProps {
   block: {
@@ -11,7 +32,20 @@ interface MarkdownTextProps {
 export default function MarkdownText({ block }: MarkdownTextProps) {
   return (
     <TypographyStylesProvider component="section" className="">
-      <Markdown remarkPlugins={[remarkGfm]}>{block.content}</Markdown>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          h1: HeadingRenderer,
+          h2: HeadingRenderer,
+          h3: HeadingRenderer,
+          h4: HeadingRenderer,
+          h5: HeadingRenderer,
+          h6: HeadingRenderer,
+        }}
+      >
+        {block.content}
+      </Markdown>
     </TypographyStylesProvider>
   );
 }
--- src/lib/component-resolver.tsx
+++ src/lib/component-resolver.tsx
@@ -19,7 +19,11 @@ export default function componentResolver(
   // See https://webpack.js.org/api/module-methods/#import-1
 
   // Use react lazy loading to import the module. By convention: The file name needs to match the name of the component (what is a good idea)
-  let componentModule = lazy(() => import(`../components/${path}`));
+  const componentModule = lazy(() =>
+    import(`../components/${path}`).catch(() => ({
+      default: () => null,
+    })),
+  );
 
   // Create react element. The 'type' argument needs to be a FunctionComponent, not a string
   const reactElement = createElement(componentModule, {
