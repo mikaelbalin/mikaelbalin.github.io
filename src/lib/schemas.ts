593111a199a2744a3acd738dd0a14bc214e1f959
Mikael Balin <michbalin@gmail.com>
2024-09-17 23:09:57 +0100

    Refactor signin and signup schemas to improve identifier and email validation

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,7 +1,5 @@
 import { z } from "zod";
 
-const email = z.string().email({ message: "Invalid email" }).min(3).max(20);
-
 const password = z
   .string({ message: "Invalid password" })
   .min(6, {
@@ -12,7 +10,14 @@ const password = z
   });
 
 export const signinSchema = z.object({
-  identifier: email,
+  identifier: z
+    .string()
+    .min(3, {
+      message: "Identifier must have at least 3 or more characters",
+    })
+    .max(20, {
+      message: "Please enter a valid username or email address",
+    }),
   password,
 });
 
@@ -20,7 +25,7 @@ export type SigninSchema = z.infer<typeof signinSchema>;
 
 export const signupSchema = z.object({
   username: z.string().min(3).max(20),
-  email,
+  email: z.string().email({ message: "Invalid email" }).min(3).max(20),
   password,
 });
 
