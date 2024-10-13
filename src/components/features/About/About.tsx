1d55407d4df0e153459434f3d827319fcc27fa39
Mikael Balin <michbalin@gmail.com>
2024-10-13 14:46:29 +0100

    Add framer-motion animation to About component for improved visibility

--- src/components/features/About/About.tsx
+++ src/components/features/About/About.tsx
@@ -1,11 +1,33 @@
+"use client";
+
 import { Container, Title, Text, Grid, GridCol } from "@mantine/core";
 import { AboutProps } from "@/types/data";
+import { motion } from "framer-motion";
 
 export const About = (props: AboutProps) => {
   const { title, description } = props;
 
   return (
-    <Container component="section" className="pt-26 pb-16 sm:pt-34 sm:pb-24">
+    <Container
+      className="pt-26 pb-16 sm:pt-34 sm:pb-24"
+      renderRoot={(props) => (
+        <motion.div
+          {...props}
+          initial={{
+            opacity: 0,
+            y: "50%",
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+            transition: {
+              duration: 1,
+            },
+          }}
+          viewport={{ once: true }}
+        />
+      )}
+    >
       <Title order={2} className="mb-8">
         {title}
       </Title>
