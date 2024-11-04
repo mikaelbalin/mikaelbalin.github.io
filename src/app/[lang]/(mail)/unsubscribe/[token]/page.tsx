086b7bdafaa527fb4eeb430d10b301ca738dcddc
Mikael Balin <michbalin@gmail.com>
2024-11-04 18:40:45 +0000

    Update unsubscribe page to confirm successful unsubscription and provide a resubscribe option

--- src/app/[lang]/(mail)/unsubscribe/[token]/page.tsx
+++ src/app/[lang]/(mail)/unsubscribe/[token]/page.tsx
@@ -1,17 +1,12 @@
-import { Title, Text, Container, Paper } from "@mantine/core";
-import Link from "next/link";
+import { Title, Text, Container, Paper, Button } from "@mantine/core";
 
 export default function SignInRoute() {
   return (
     <Container size="sm" className="my-16 sm:my-19.5">
       <Paper withBorder className="p-8 shadow-lg flex flex-col gap-6">
-        <Title size="h3">Welcome back!</Title>
-        <Text c="dimmed" size="sm">
-          Enter your details to sign in to your account
-        </Text>
-        <Text>
-          Do not have an account yet? <Link href="/signup">Create account</Link>
-        </Text>
+        <Title size="h3">Successfully unsubscribed</Title>
+        <Text>You have been unsubscribed from future emails.</Text>
+        <Button>Subscribe back</Button>
       </Paper>
     </Container>
   );
