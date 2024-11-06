14febf2b1188ae80f66c2646e40f4894a838a006
Mikael Balin <michbalin@gmail.com>
2024-11-06 21:42:07 +0000

    Migrate ESLint configuration to new format and update dependencies for improved compatibility

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat";
+import storybook from "eslint-plugin-storybook";
+import tseslint from "typescript-eslint";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  ...storybook.configs["flat/recommended"],
+  ...fixupConfigRules(compat.extends("next/core-web-vitals")),
+  {
+    ignores: [".next/*", "node_modules/*"],
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
+);
