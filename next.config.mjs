0211152e3f7bf5d8ff5444d1eefd8900d7177bd6
Mikael Balin <michbalin@gmail.com>
2024-12-06 12:52:03 +0000

    Refactor Next.js configuration to streamline server URL handling and reorganize MDX and bundle analyzer integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
-// @ts-check
-
 import nextBundleAnalyzer from "@next/bundle-analyzer";
 import nextMDX from "@next/mdx";
 import { withPayload } from "@payloadcms/next/withPayload";
 
-const withMDX = nextMDX({
-  options: {
-    baseUrl: import.meta.url,
-  },
-});
-
-const withBundleAnalyzer = nextBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -31,17 +23,27 @@ const nextConfig = {
         port: "1337",
         pathname: "/uploads/**/*",
       },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
+      ...[NEXT_PUBLIC_SERVER_URL, "https://placehold.co"].map((item) => {
+        const url = new URL(item);
+
+        return {
+          protocol: url.protocol.replace(":", ""),
+          hostname: url.hostname,
+        };
+      }),
     ],
   },
   reactStrictMode: true,
 };
+
+const withMDX = nextMDX({
+  options: {
+    baseUrl: import.meta.url,
+  },
+});
+
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 export default withBundleAnalyzer(withMDX(withPayload(nextConfig)));
