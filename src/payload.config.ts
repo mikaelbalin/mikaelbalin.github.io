ccabf9e8fc4b75f7aa57fb0216d0d0fc6e492c97
Mikael Balin <michbalin@gmail.com>
2024-12-29 18:59:08 +0000

    Refactor payload configuration: update Pages import path to use the new directory structure

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -11,9 +11,9 @@ import { header } from "@/config/header";
 import { getServerSideURL } from "./utilities/getURL";
 import { Categories } from "./collections/Categories";
 import { Media } from "./collections/Media";
-import { Pages } from "./collections/Pages";
 import { Posts } from "./collections/Posts";
 import { Users } from "./collections/Users";
+import { Pages } from "@/config/collections/pages";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 const filename = fileURLToPath(import.meta.url);
