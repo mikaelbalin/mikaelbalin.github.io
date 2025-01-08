8c74dcdb72a6fa6676d49487937333e6db75a0d2
Mikael Balin <michbalin@gmail.com>
2025-01-08 11:24:05 +0000

    Move BeforeLogin component to admin directory and update references; remove obsolete ui component files

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -25,7 +25,7 @@ export default buildConfig({
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
-      beforeLogin: ["@/components/ui/BeforeLogin"],
+      beforeLogin: ["@/components/admin/BeforeLogin"],
       // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
       beforeDashboard: [],
