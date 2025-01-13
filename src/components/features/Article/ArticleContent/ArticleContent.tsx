f1b4a8802ad3e4347eda00dfbbd7a18aefe0ff08
Mikael Balin <michbalin@gmail.com>
2025-01-13 17:32:59 +0000

    Refactor RichText component: move to separate file and update import structure

--- src/components/features/Article/ArticleContent/ArticleContent.tsx
+++ src/components/features/Article/ArticleContent/ArticleContent.tsx
@@ -4,7 +4,7 @@ import { useRef } from "react";
 import { Container, Grid, GridCol } from "@mantine/core";
 import { ProgressIndicator } from "@/components/ui/ProgressIndicator";
 import { Post } from "@/types/payload";
-import RichText from "@/components/ui/RichText";
+import { RichText } from "@/components/ui/RichText";
 
 type ArticleContentProps = {
   content: Post["content"];
--- src/components/ui/RichText/RichText.tsx
+++ src/components/ui/RichText/RichText.tsx
@@ -10,7 +10,7 @@ type RichTextProps = {
   htmlElement?: ElementType;
 };
 
-const RichText: FC<RichTextProps> = ({
+export const RichText: FC<RichTextProps> = ({
   className,
   textClassName,
   content,
@@ -41,5 +41,3 @@ const RichText: FC<RichTextProps> = ({
     </Tag>
   );
 };
-
-export default RichText;
