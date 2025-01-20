ea87ffcc0a986a918193650f02a141b0221f8cda
Mikael Balin <michbalin@gmail.com>
2025-01-20 20:29:15 +0000

    Add ProgressIndicator component stories for Storybook documentation

---
+++ src/components/ui/ProgressIndicator/ProgressIndicator.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ProgressIndicator } from "./ProgressIndicator";
+import { useRef } from "react";
+
+const meta = {
+  title: "ProgressIndicator",
+  component: ProgressIndicator,
+} satisfies Meta<typeof ProgressIndicator>;
+
+export default meta;
+type Story = StoryObj<typeof ProgressIndicator>;
+
+export const Default: Story = {
+  render: function Render() {
+    const ref = useRef<HTMLDivElement>(null);
+    return (
+      <div>
+        <div className="bg-[var(--mantine-color-blue-light)] w-full h-10 mb-2" />
+        <div className="flex">
+          <div
+            className="bg-[var(--mantine-color-blue-light)] w-32 h-[300px]"
+            ref={ref}
+          />
+          <div className="px-2">
+            <ProgressIndicator target={ref} />
+          </div>
+        </div>
+        <div className="bg-[var(--mantine-color-blue-light)] w-full h-[500px] mt-2" />
+      </div>
+    );
+  },
+};
