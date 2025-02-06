8a07335bf54a8feb4f594ddc8722296f03e88942
Mikael Balin <michbalin@gmail.com>
2025-02-06 16:07:44 +0000

    Rename PageTemplate to Page in main page component

--- src/app/(frontend)/[lang]/(main)/page.tsx
+++ src/app/(frontend)/[lang]/(main)/page.tsx
@@ -1,5 +1,5 @@
-import PageTemplate, { generateMetadata } from "./[slug]/page";
+import Page, { generateMetadata } from "./[slug]/page";
 
-export default PageTemplate;
+export default Page;
 
 export { generateMetadata };
