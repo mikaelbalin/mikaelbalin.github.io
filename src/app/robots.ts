26aa88b359d7724aadb661915a8cf1c7b25c51be
Mikael Balin <michbalin@gmail.com>
2025-02-09 12:06:03 +0000

    Update robots.txt to allow access to Open Graph routes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      allow: ["/", "/next/og/*"],
       disallow: "/admin/",
     },
     sitemap: `${getServerSideURL()}/sitemap.xml`,
