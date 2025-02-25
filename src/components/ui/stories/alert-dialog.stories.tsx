0b25c24d3a624db77dbf40ec8328721fc8535bd7
Mikael Balin <michbalin@gmail.com>
2025-02-25 22:36:09 +0000

    Add AlertDialog and Button components with Storybook stories; update package.json for Radix UI Alert Dialog

---
+++ src/components/ui/stories/alert-dialog.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from "@storybook/react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+export default {
+  title: "shadcn/Alert Dialog",
+  component: Button,
+  tags: ["autodocs"],
+} satisfies Meta<typeof AlertDialog>;
+
+type Story = StoryObj<typeof AlertDialog>;
+
+export const Default: Story = {
+  args: {},
+  render: (args) => {
+    return (
+      <AlertDialog {...args}>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
+  },
+};
