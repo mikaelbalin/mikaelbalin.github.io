3300b050353dbaaee4391d4cc28f715bc1e1bd28
Mikael Balin <michbalin@gmail.com>
2025-03-03 23:47:35 +0000

    Clear migrations array by removing all entries

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20250303_232905 from './20250303_232905';
-
-export const migrations = [
-  {
-    up: migration_20250303_232905.up,
-    down: migration_20250303_232905.down,
-    name: '20250303_232905'
-  },
-];
+export const migrations = [];
