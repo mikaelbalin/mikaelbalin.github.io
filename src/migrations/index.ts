55a8bd2d9fe4e065228916e3de9d05ddeb0d43e0
Mikael Balin <michbalin@gmail.com>
2025-03-03 22:50:20 +0000

    Add migration for new title columns in pages and posts tables

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250214_131305 from './20250214_131305';
+import * as migration_20250303_225008 from './20250303_225008';
 
 export const migrations = [
   {
     up: migration_20250214_131305.up,
     down: migration_20250214_131305.down,
-    name: '20250214_131305'
+    name: '20250214_131305',
+  },
+  {
+    up: migration_20250303_225008.up,
+    down: migration_20250303_225008.down,
+    name: '20250303_225008'
   },
 ];
