fe6ba71d05a43167278a5b1046ababbdaa999228
Mikael Balin <michbalin@gmail.com>
2025-03-04 08:09:49 +0000

    Add migration entry for 20250304_080943 to migrations array

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,1 +1,9 @@
-export const migrations = [];
+import * as migration_20250304_080943 from './20250304_080943';
+
+export const migrations = [
+  {
+    up: migration_20250304_080943.up,
+    down: migration_20250304_080943.down,
+    name: '20250304_080943'
+  },
+];
