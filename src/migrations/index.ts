ea3874dfbd987893fd7c1db3dd0d9975b71a24e7
Mikael Balin <michbalin@gmail.com>
2025-03-04 09:14:03 +0000

    Update migration reference to use new migration version 20250304_091316

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250304_080943 from './20250304_080943';
+import * as migration_20250304_091316 from './20250304_091316';
 
 export const migrations = [
   {
-    up: migration_20250304_080943.up,
-    down: migration_20250304_080943.down,
-    name: '20250304_080943'
+    up: migration_20250304_091316.up,
+    down: migration_20250304_091316.down,
+    name: '20250304_091316'
   },
 ];
