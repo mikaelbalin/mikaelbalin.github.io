144b694a764418e868b82e7dd0d23675cab5ca24
Mikael Balin <michbalin@gmail.com>
2025-03-05 11:24:18 +0000

    Update error and not-found pages to add max-width and padding for better layout

--- src/app/(frontend)/[lang]/error.tsx
+++ src/app/(frontend)/[lang]/error.tsx
@@ -6,7 +6,7 @@ import { Title, Text } from "@mantine/core";
 export default function ErrorPage({ error }: { error: Error }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="space-y-4">
+      <div className="space-y-4 max-w-80 px-4">
         <IconBugFilled className="h-36 w-36" />
         <Title size="h3">Oops! Something went wrong.</Title>
         <Text>This is an error page. Please try again later.</Text>
