b70f6473af5c3b61c4cb431188602fdabbcadb3c
Mikael Balin <michbalin@gmail.com>
2025-03-05 09:13:07 +0000

    Refactor migration script to remove conditional column additions and streamline down migration

--- src/migrations/20250304_112448.ts
+++ src/migrations/20250304_112448.ts
@@ -6,22 +6,6 @@ import {
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-  DO $$ 
-   BEGIN 
-     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='pages' AND column_name='title') THEN
-       ALTER TABLE "pages" ADD COLUMN "title" varchar;
-     END IF;
-     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='_pages_v' AND column_name='version_title') THEN
-       ALTER TABLE "_pages_v" ADD COLUMN "version_title" varchar;
-     END IF;
-     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='posts' AND column_name='title') THEN
-       ALTER TABLE "posts" ADD COLUMN "title" varchar;
-     END IF;
-     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='_posts_v' AND column_name='version_title') THEN
-       ALTER TABLE "_posts_v" ADD COLUMN "version_title" varchar;
-     END IF;
-   END $$;
-
   ALTER TABLE "pages_locales" DROP COLUMN IF EXISTS "title";
   ALTER TABLE "_pages_v_locales" DROP COLUMN IF EXISTS "version_title";
   ALTER TABLE "posts_locales" DROP COLUMN IF EXISTS "title";
@@ -37,9 +21,5 @@ export async function down({
   ALTER TABLE "pages_locales" ADD COLUMN "title" varchar;
   ALTER TABLE "_pages_v_locales" ADD COLUMN "version_title" varchar;
   ALTER TABLE "posts_locales" ADD COLUMN "title" varchar;
-  ALTER TABLE "_posts_v_locales" ADD COLUMN "version_title" varchar;
-  ALTER TABLE "pages" DROP COLUMN IF EXISTS "title";
-  ALTER TABLE "_pages_v" DROP COLUMN IF EXISTS "version_title";
-  ALTER TABLE "posts" DROP COLUMN IF EXISTS "title";
-  ALTER TABLE "_posts_v" DROP COLUMN IF EXISTS "version_title";`);
+  ALTER TABLE "_posts_v_locales" ADD COLUMN "version_title" varchar;`);
 }
