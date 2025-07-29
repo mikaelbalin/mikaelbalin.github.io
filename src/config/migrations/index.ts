09cd553aae651e4ae81933c2d530fb551a7cd7dd
Mikael Balin <michbalin@gmail.com>
2025-07-29 22:06:33 +0100

    feat(migrations): add users_sessions table and update redirects index
    - Created a new table "users_sessions" with columns for order, parent ID, session ID, created at, and expiration timestamp.
    - Established a foreign key constraint on "_parent_id" referencing the "users" table.
    - Added indexes for performance optimization on "_order" and "_parent_id".
    - Updated the default subject for "forms_emails_locales".
    - Dropped and recreated the "redirects_from_idx" index.

--- src/config/migrations/index.ts
+++ src/config/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250406_141334 from './20250406_141334';
 import * as migration_20250414_171557 from './20250414_171557';
 import * as migration_20250414_174811 from './20250414_174811';
+import * as migration_20250729_210554 from './20250729_210554';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20250414_174811.up,
     down: migration_20250414_174811.down,
-    name: '20250414_174811'
+    name: '20250414_174811',
+  },
+  {
+    up: migration_20250729_210554.up,
+    down: migration_20250729_210554.down,
+    name: '20250729_210554'
   },
 ];
