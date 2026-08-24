f21a8a5f8826302342f5cf33a6efb53a9cb0b8c0
Mikael Balin <hello@mikaelbalin.com>
2026-08-24 17:20:49 +0100

    Remove unused SVG icon and its head link reference

--- src/app/(frontend)/[lang]/layout.tsx
+++ src/app/(frontend)/[lang]/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout(props: LayoutProps<"/[lang]">) {
     <html lang={lang} className="relative" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        {/*<link rel="icon" href="/icon.svg" type="image/svg+xml" />*/}
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta
           name="viewport"
